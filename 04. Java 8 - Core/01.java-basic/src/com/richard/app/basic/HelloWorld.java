2afe1208125995e6e69c6f5c3ba5da42892deb1e
Richard <richardvu.work@gmail.com>
2019-10-15 11:24:26 +0700

    Update Java 8 Core

---
+++ 04. Java 8 - Core/01.java-basic/src/com/richard/app/basic/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * -----Coder's House Teams-----
+ * Skype: luongvu.work@gmail.com
+ * Mail: codershouseteams@gmail.com
+ * Phone Number: (+84) 0935710974 - (+84) 0935810974
+ * Project Name: 01.java-language-basics
+ * Create Date: Oct 15, 2019
+ */
+package com.richard.app.basic;
+
+/**
+ * @author richard
+ *
+ */
+public class HelloWorld {
+	public void helloWorld() {
+		System.out.println("Hello World!!!");
+	}
+}
